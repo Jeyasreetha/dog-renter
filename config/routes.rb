199af33a9f2a7b15a98b9d7c8a1aa2b4d1cc4d89
Olga Voronina <o.d.vrnn@gmail.com>
2019-10-29 05:07:48 -0400

    worked on /dashboard page and /users/edit page

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # profile page for the users (owners and renters) ???
-  get 'dashboard', to:'users#dashboard'
-
+  get 'dashboard', to:'pages#dashboard'
+  get 'dashboard/edit', to: "pages#dashboard_edit"
   # CRUD
   resources :dogs do
   # If only there is a dog you could rent
