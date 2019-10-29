0f3fed6cc10890fb9c8ed1891bb14c7ca480d058
Elise Wetherholt <ewether@gmail.com>
2019-10-29 16:39:06 +0800

    finished rentings controller

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -1,13 +1,20 @@
 class RentingsController < ApplicationController
   def create
     @renting = Renting.new(renting_params)
-    @dog.user = current_user
-    @dog = dog.find(params[:id])
+    @dog = Dog.find(params[:dog_id])
+    @renting.dog = @dog
+    @renting.user = current_user
+    @renting.total_price = (@renting.end - @renting.start) / 86_400 * @dog.price
+    if @renting.save
+      redirect_to dashboard_path
+    else
+      render 'dogs/show'
+    end
   end
 
   private
 
   def renting_params
-    params.require(:renting).permit(:total_price, :start_date, :end_date)
+    params.require(:renting).permit(:start, :end)
   end
 end
