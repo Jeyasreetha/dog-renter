927153b6efb20937bd1882d7e7da27ff0223b796
Jeyasreetha PArameswaran <jeyasreetha@googlemail.com>
2019-10-29 17:34:44 +0800

    working on dog policy pundit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,8 @@
 class DogsController < ApplicationController
+  # call the method set_dog before selected actions
   before_action :set_dog, only: [:show, :edit, :update, :destroy]
+  # No need to be logged-in
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     # Read all dogs
@@ -9,28 +12,37 @@ class DogsController < ApplicationController
   def show
     # Read one dog
     # @dog = Dog.find(params[:id])
+
+    # call the dog_policy method show
+    authorize @dog
   end
 
   def new
     # opens an empty form
     @dog = Dog.new
+    # calls the dog_policy method new
+    authorize @dog
   end
 
   def create
     # Create a new instance with params
     @dog = Dog.new(dog_params)
+    authorize @dog
+
     # Need to be logged-in to add dog listing
     @dog.user = current_user
     if @dog.save
       # Go to the show page of the created dog
       redirect_to dog_path(@dog)
     else
-
+      #
       render 'new'
     end
   end
 
   def edit
+    # calls the dog_policy method edit
+    authorize @dog
     # renders a form already with info to change
     # @dog = Dog.find(params[:id])
   end
@@ -39,12 +51,15 @@ class DogsController < ApplicationController
     # Get the dog you want to edit
     # @dog = Dog.find(params[:id])
     # render the form with the info to update
+    authorize @dog
     @dog.update(dog_params)
+
     # Go to the show page of the updated dog
     redirect_to dog_path(@dog)
   end
 
   def destroy
+    authorize @dog
     # Get the dog you want to destroy
     # @dog = Dog.find(params[:id])
     # delete the dog
