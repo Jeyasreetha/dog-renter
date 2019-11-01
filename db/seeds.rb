5528f082fc5ad936b6002386f523809019a84582
Olga Voronina <o.d.vrnn@gmail.com>
2019-11-01 00:26:35 -0400

    added dogs to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,25 +12,67 @@ Renting.destroy_all
 Dog.destroy_all
 User.destroy_all
 
-puts 'Creating 10 fake users and dogs'
-10.times do
-   user = User.new(
-    name: Faker::Name.unique.name,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password
-   )
-   user.save!
-# puts 'Now creating 10 fake dogs'
-    dog = Dog.new(
-    user: user,
-    name: Faker::Dessert.flavor,
-    age: Faker::Number.within(range: 1..10),
-    breed: Faker::Food.fruits,
-    price: Faker::Number.within(range: 5..100),
-    location: "#{Faker::Address.street_address}, #{Faker::Address.city}"
-  )
-  dog.save!
-end
-puts 'Finished creating 10 fake users and dogs'
+puts 'creating user'
+
+user = User.new(name: 'Olga', email: 'olga@gmail.com', location: "Canguu", password: "password")
+user.save
+
+user = User.new(name: 'Jesh', email: 'jesh@gmail.com', location: "Seminjak" , password: "password")
+user.save
+
+
+puts 'users created'
+
+puts 'Creating dogs'
+
+url = "https://images.pexels.com/photos/133069/pexels-photo-133069.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Spotty', age: "1", price: "15", location: "Berawa", user_id: User.all.sample.id, breed: "French Bulldog")
+dog.remote_image_url = url
+dog.save
+
+url = "https://images.pexels.com/photos/57627/pexels-photo-57627.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Bruno', age: "5", price: "10", location: "Canggu", user_id: User.all.sample.id, breed: "Poodle")
+dog.remote_image_url = url
+dog.save
+
+url = "https://images.pexels.com/photos/1322182/pexels-photo-1322182.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Jane', age: "1", price: "34", location: "Seminjak", user_id: User.all.sample.id, breed: "Australian Shepherd")
+dog.remote_image_url = url
+dog.save
+
+url = "https://images.pexels.com/photos/1805165/pexels-photo-1805165.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Heroku', age: "3", price: "22", location: "Canggu", user_id: User.all.sample.id, breed: "Labrador")
+dog.remote_image_url = url
+dog.save
+
+url = "https://images.pexels.com/photos/776078/pexels-photo-776078.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Juuu', age: "2", price: "30", location: "Kuta", user_id: User.all.sample.id, breed: "French Bulldog")
+dog.remote_image_url = url
+dog.save
+
+url = "https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Bianco', age: "4", price: "35", location: "Seminjak", user_id: User.all.sample.id, breed: "Unknown")
+dog.remote_image_url = url
+dog.save
+
+url = "https://images.pexels.com/photos/1753079/pexels-photo-1753079.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Foo', age: "1", price: "30", location: "Canggu", user_id: User.all.sample.id, breed: "Unknown")
+dog.remote_image_url = url
+dog.save
+
+url = "https://images.pexels.com/photos/976871/pexels-photo-976871.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Foxy', age: "4", price: "25", location: "Seminjak", user_id: User.all.sample.id, breed: "Unknown")
+dog.remote_image_url = url
+dog.save
+
+url = "https://images.pexels.com/photos/374906/pexels-photo-374906.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=400"
+dog = Dog.new(name: 'Joe', age: "5", price: "20", location: "Seminjak", user_id: User.all.sample.id, breed: "Unknown")
+dog.remote_image_url = url
+dog.save
+
+
+
+puts 'dogs created'
+
 
 
